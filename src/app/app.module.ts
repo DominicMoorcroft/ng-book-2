cb227527e4807f597d07bd0d477dc413cee6e0f4
Dominic Moorcroft <dominic.moorcroft@hermes-europe.co.uk>
2017-06-21 17:05:36 +0100

    Up to last bit of chapter 5, have generated the new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent, SidebarConponent, ArticleComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { ArticleComponent } from './article/article.component';
+import { ArticleListComponent } from './article-list/article-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarConponent,
-    ArticleComponent
+    ArticleComponent,
+    SidebarComponent,
+    ArticleComponent,
+    ArticleListComponent
   ],
   imports: [
     BrowserModule,
